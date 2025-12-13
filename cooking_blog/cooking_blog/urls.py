0bbe8aca8c9198001db7792179537f9457dfcceb
Dure Matias Miguel <72953094+MatiaSalvatore@users.noreply.github.com>
2025-12-13 00:32:36 -0300

    Formulario de contacto terminado

--- cooking_blog/cooking_blog/urls.py
+++ cooking_blog/cooking_blog/urls.py
@@ -24,7 +24,8 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',index, name='index'),
-    path('',include('apps.posts.urls'))
+    path('',include('apps.posts.urls')),
+    path('',include('apps.contacto.urls')),
 ]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
 urlpatterns += staticfiles_urlpatterns() 
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
